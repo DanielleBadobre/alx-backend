191a90e83f1688927dfd5d052bc24a9cd097f961
root <root@DESKTOP-8H5NMFH.>
2024-10-29 08:24:54 +0000

    not done

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" 0-simple_helper_function """
+
+
+def index_range(page, page_size):
+    """ function named index_range that takes two integer arguments"""
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (star, end)
+
